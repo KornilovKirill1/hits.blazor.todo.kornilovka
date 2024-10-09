f6e262c11ddfd52ebe9eaf4735fab8fd092574d9
kilianAid <kilian_bs@mail.ru>
2024-10-09 18:25:46 +0500

    Task3

--- TodoServerApp/Data/ApplicationDbContext.cs
+++ TodoServerApp/Data/ApplicationDbContext.cs
@@ -5,5 +5,17 @@ namespace TodoServerApp.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
-    }
+        public virtual DbSet<TaskItem> TaskItems { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
+			builder.Entity<TaskItem>().HasData([
+					new() { Id = 1, Title = "Задача 1", Description = "Описание задача 1", CreatedDate =  DateTime.Now},
+					new() { Id = 2, Title = "Задача 2", Description = "Описание задача 2", CreatedDate =  DateTime.Now},
+					new() { Id = 3, Title = "Задача 3", Description = "Описание задача 3", CreatedDate =  DateTime.Now},
+					new() { Id = 4, Title = "Задача 4", Description = "Описание задача 4", CreatedDate =  DateTime.Now},
+				]);
+		}
+	}
 }
