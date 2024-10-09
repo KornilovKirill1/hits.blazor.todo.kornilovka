18fb259eba9cbffd3384bc5ca366435b3876b30a
kilianAid <kilian_bs@mail.ru>
2024-10-09 20:05:06 +0500

    Task5 Adding functions "Add","Delete","Change"

--- TodoServerApp/Data/Services/MemoryDataService.cs
+++ TodoServerApp/Data/Services/MemoryDataService.cs
@@ -11,10 +11,25 @@ namespace TodoServerApp.Data.Services
 			new() { Id = 4, Title = "Задача 4", Description = "Описание задача 4", CreatedDate =  DateTime.Now},
 		];
 
-        public async Task<IEnumerable<TaskItem>> GetTaskItemsAsync()
+		public Task DeleteAsync(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<TaskItem> GetTaskAsync(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<IEnumerable<TaskItem>> GetAllAsync()
         {
             await Task.Delay(1000);
             return await Task.FromResult(Tasks);
         }
-    }
+
+		public Task SaveAsync(TaskItem item)
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
